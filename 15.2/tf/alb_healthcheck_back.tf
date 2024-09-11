bbe0d334a1286644dcaa4f12d62928464d9b0a24
caevert <caevert@gmail.com>
2024-09-11 09:14:14 +0300

    Update alb_healthcheck_back.tf

--- 15.2/tf/alb_healthcheck_back.tf
+++ 15.2/tf/alb_healthcheck_back.tf
@@ -1,4 +1,4 @@
-resource "yandex_alb_backend_group" "backend-group" {
+1resource "yandex_alb_backend_group" "backend-group" {
   name                     = "backend-balancer"
   session_affinity {
     connection {
